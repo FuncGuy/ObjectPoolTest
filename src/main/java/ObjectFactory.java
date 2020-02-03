43ec37d2ebe2fd1dac23da9857735521c5b75874
FuncGuy <tim25659@gmail.com>
2020-02-03 19:24:47 +0530

    Initial commit

---
+++ src/main/java/ObjectFactory.java
@@ -0,0 +1,9 @@
+public interface ObjectFactory<T> {
+
+    /**
+     * Returns a new instance of an object type T.
+     *
+     * @return T an new instance of the object type T
+     */
+      T createNew();
+}
---
+++ src/main/java/Pool.java
@@ -0,0 +1,17 @@
+public interface Pool<T> {
+
+    /*
+     * @return one of the pooled objects.
+     */
+    T get();
+
+    /*
+     * @param object T to be return back to pool
+     */
+    void release(T object);
+
+    /**
+     * Shuts down the pool. Should release all resources.
+     */
+    void shutdown();
+}
